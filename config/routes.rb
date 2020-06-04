86e396d00108e5bc109b0d9c00fd6e0dedc197d8
zizynonno <zizynonno@gmail.com>
2020-06-04 23:50:19 +0900

    keyword view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :authors
   resources :users
   resources :books
+  get 'view/keyword'
+  post 'keyword/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
